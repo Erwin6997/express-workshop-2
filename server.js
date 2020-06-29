78b4c5f8d136d1dd746a748877c341c06d93e1d2
Erwin6997 <59776155+Erwin6997@users.noreply.github.com>
2020-06-29 10:28:48 +0100

    13

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const app = express();
 // i.e /my-cv will server /my-cv.html
 app.use(express.static("public", {'extensions': ['html']}));
 
+// Add this to the top of your file
+const exphbs = require("express-handlebars");
 
+// Then these two lines after you initialise your express app
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 // what does this line mean: process.env.PORT || 3000
 app.listen(process.env.PORT || 3000, function () {
   console.log("Server is listening on port 3000. Ready to accept requests!");
